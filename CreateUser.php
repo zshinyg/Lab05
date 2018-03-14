8ecc97ff175b23f1f0b031040fed490e7180f152
Coltyn Gatton <c527g802@ku.edu>
2018-03-14 18:36:58 -0500

    Added error check

--- CreateUser.php
+++ CreateUser.php
@@ -1,4 +1,7 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+    
     $mysqli = new mysqli("mysql.eecs.ku.edu", "c527g802", "rotaHea4", "c527g802");
 
     /* check connection */
