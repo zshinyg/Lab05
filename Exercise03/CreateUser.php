a7ddd4fab6ce2d02720b438afd0b5a3ae71b164e
Coltyn Gatton <c527g802@ku.edu>
2018-03-14 16:34:10 -0500

    Added files

---
+++ Exercise03/CreateUser.php
@@ -0,0 +1,15 @@
+<?php
+    $mysq = new mysqli("mysql.eecs.ku.edu", "c527g802", "rotaHea4", "c527g802");
+
+    /* check connection */
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+
+    
+
+
+
+
+?>
