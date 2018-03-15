0f8838240af635b9f8a7255834e53b9ae5d69a8f
Coltyn Gatton <c527g802@ku.edu>
2018-03-14 19:21:31 -0500

    CreatePost

---
+++ CreatePosts.php
@@ -0,0 +1,51 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+    $mysqli = new mysqli("mysql.eecs.ku.edu", "c527g802", "rotaHea4", "c527g802");
+
+    /* check connection */
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+
+    $userName = $_POST['userName'];
+    $checkUsername = $mysqli->query($query);
+    $insertUsername = "INSERT INTO User (user_id)
+    VALUES ('$userName')";
+
+
+    $userPost = $_POST['userPost'];
+    $insertPost = "INSERT INTO Posts (content)
+    VALUES ('$userPost')";
+    $query = "SELECT content FROM Post WHERE content = '$userPost'";
+
+
+    if($userName == ""){
+        echo "<div class='alert alert-danger' role='alert'>";
+        echo "<strong>Invalid Username</strong>";
+        echo "</div>";
+        die();
+    } elseif (($checkUsername->num_rows == 0)) {
+        echo "<div class='alert alert-danger' role='alert'>";
+        echo "<strong>Username does not exist</strong>";
+        echo "</div>";
+        die();
+    }
+
+    if($userPost == ""){
+        echo "You must post something.";
+        die();
+    }
+    
+
+    if ($mysqli->query($insertPost) === TRUE) {
+        echo "New user created successfully";
+    } else {
+        echo "Error: " . $insertUsername . "<br>" . $mysqli->error;
+    }
+
+    $mysqli->close();
+
+?>
