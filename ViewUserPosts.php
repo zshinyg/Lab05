3b03ab65099623f2d702234666b5c1f8254ef2a4
Coltyn Gatton <c527g802@ku.edu>
2018-03-15 21:16:35 -0500

    Added post php

---
+++ ViewUserPosts.php
@@ -0,0 +1,39 @@
+<?php 
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+    $conn = new mysqli("mysql.eecs.ku.edu", "c527g802", "rotaHea4", "c527g802");
+
+    /* check connection */
+    if ($conn->connect_errno) {
+        printf("Connect failed: %s\n", $conn->connect_error);
+        exit();
+    }
+
+    $table = "SELECT conten FROM Posts";
+    $tableResult = $conn->query($table);
+    $user = $_POST['userSelect'];
+
+    echo "<link rel='stylesheet' href='styles.css' type='text/css'>";
+
+    echo "<table class='userTable'>";
+    echo "<tr>";
+    echo "<th>Posts</th>";
+    echo "<tbody>";
+    echo "</tr>";
+
+    if($tableResult->num_rows > 0){
+        while($row = $tableResult->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td align='center'>" . $row['content'] . "</td>";
+            echo "</tr>";
+        }
+    } else {
+        echo "No posts have been created.";
+    }
+
+    echo "</tbody>";
+    echo "</table>";
+
+    $conn->close();
+?>
